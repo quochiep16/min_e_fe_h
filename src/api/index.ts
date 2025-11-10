38853148ad3cfaa1c21e4b06631e9674c00f73c9
quochiep16 <quochiep1610@gmail.com>
2025-11-11 02:45:10 +0700

    fe ci/cicd

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = (import.meta as any).env?.VITE_API_URL || '/api';
+// src/... 
+const API_BASE_URL = 'http://192.168.1.199:3000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
