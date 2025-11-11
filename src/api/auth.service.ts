2cbe5fe4b46ee89e7d216e2802dc08ac81d1e02e
quochiep16 <quochiep1610@gmail.com>
2025-11-11 10:02:28 +0700

    demo

--- src/api/auth.service.ts
+++ src/api/auth.service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // src/... 
-const API_BASE_URL = 'http://192.168.1.199:3000/api';
+const API_BASE_URL = 'http://172.17.2.199:3000/api';
 
 
 const api = axios.create({
